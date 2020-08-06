35b8dbec4b0f0a90ce62bfde6a6f75b098103853
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-08-06 17:22:40 +0700

    value object

---
+++ lib/domain/core/value_object.dart
@@ -0,0 +1,21 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_firebase_ddd_aug/domain/core/value_failures.dart';
+
+abstract class ValueObject<T> {
+  const ValueObject();
+  Either<ValueFailure<T>, T> get value;
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+
+    return o is ValueObject<T> && o.value == value;
+  }
+
+  @override
+  int get hashCode => value.hashCode;
+
+  @override
+  String toString() => 'Value($value)';
+
+}
