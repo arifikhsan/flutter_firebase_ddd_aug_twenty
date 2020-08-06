55e145c4fd95d0957b0662279052dd6110a1f4a5
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-08-06 22:32:23 +0700

    firebase auth and google signin

--- lib/domain/core/value_object.dart
+++ lib/domain/core/value_object.dart
@@ -1,11 +1,17 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_firebase_ddd_aug/domain/core/errors.dart';
 import 'package:flutter_firebase_ddd_aug/domain/core/value_failures.dart';
 
 @immutable
 abstract class ValueObject<T> {
   const ValueObject();
   Either<ValueFailure<T>, T> get value;
+
+  /// Throws [UnexpectedValueError]
+  T getOrCrash() {
+    return value.fold((failure) => throw UnexpectedValueError(failure), id);
+  }
 
   bool isValid() => value.isRight();
 
@@ -21,5 +27,4 @@ abstract class ValueObject<T> {
 
   @override
   String toString() => 'Value($value)';
-
 }
